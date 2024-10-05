c3063f194270f6bd344c1d4b49ba5c6f0f319a3b
beniihime <carpul8089@gmail.com>
2024-10-05 20:23:03 +0600

    modify && fix redirect

--- src/router/index.js
+++ src/router/index.js
@@ -13,17 +13,18 @@ const routes = [
                 path: "/users",
                 component: () => import('@/views/UsersPage.vue'),
                 name: 'Users',
-                meta: { permission: { type: 'User', action: 'Read' } }
+                meta: { permission: { type: 'User', action: 'Read' }, requiresAuth: true }
             },
             {
                 path: "/notif",
                 component: () => import('@/views/NotifPage.vue'),
+                meta: { requiresAuth: true  }
             },
             {
                 path: "/rbac",
                 component: () => import('@/views/RbacPage.vue'),
                 name: 'Rbac',
-                meta: { permission: { type: 'Rbac', action: 'Read' } }
+                meta: { permission: { type: 'Rbac', action: 'Read' }, requiresAuth: true  }
             },
             {
                 path: "/me-permissions",
@@ -34,18 +35,19 @@ const routes = [
                 path: "/role-permissions",
                 component: () => import('@/views/RolePermissionsPage.vue'),
                 name: 'RolePermissions',
-                meta: { permission: { type: 'Rbac', action: 'Create' } }
+                meta: { permission: { type: 'Rbac', action: 'Create' }, requiresAuth: true  }
             },
             {
                 path: "/profile",
                 component: () => import('@/views/ProfilePage.vue'),
                 name: 'Profile',
+                meta: { requiresAuth: true  }
             },
             {
                 path: "/services",
                 component: () => import('@/views/ServicesPage.vue'),
                 name: 'Services',
-                meta: { permission: { type: 'InfraManager', action: 'Read' } }
+                meta: { permission: { type: 'InfraManager', action: 'Read' }, requiresAuth: true  }
             },
         ]
     }, 
@@ -59,6 +61,15 @@ const routes = [
         path: '/noAccess',
         component: () => import('@/components/Utils/PermissionDenied.vue'),
         name: 'NoAccess'
+    },
+    {
+        path: '/notFound',
+        component: () => import('@/components/Utils/NotFound.vue'),
+        name: 'NotFound'
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        redirect: '/notFound'
     }
 ];
 
@@ -69,11 +80,15 @@ const router = createRouter({
 
 router.beforeEach(async (to, from) => {
     const permissionStore = usePermissionStore();
+
+    if (to.path === '/auth' && isAuthenticated()) {
+        return { path: '/' };  // Перенаправляем на главную страницу
+    }
     
     // Если маршрут требует полномочий
     if (to.meta.permission) {
         try {
-            await permissionStore.fetchPermissions();  // Ждём загрузки полномочий
+              // Ждём загрузки полномочий
         } catch (error) {
             console.error('Ошибка при загрузке полномочий:', error);
             return { path: '/auth' };  // Если ошибка при загрузке, отправляем на страницу авторизации
@@ -94,7 +109,7 @@ router.beforeEach(async (to, from) => {
         if (!isAuthenticated()) {
             return {
                 path: '/auth',
-                query: { redirect: to.fullPath }
+                replace: true
             };
         }
     }
@@ -102,6 +117,19 @@ router.beforeEach(async (to, from) => {
     // Разрешаем навигацию
     return true;
 });
+
+router.afterEach((to) => {
+    if (to.meta.requiresAuth) {
+        if (!isAuthenticated()) {
+            if (window) {
+                window.history.replaceState(null, '', window.location.href);
+                window.onpopstate = () => {
+                    window.history.forward();
+                }
+            }
+        }
+    }
+})
 
 
 export default router;
